9d1854d87ddb4d71007b8f55efdd1a178d457bbd
hanisibr <51124192+hanisibr@users.noreply.github.com>
2020-12-27 14:09:48 +0000

    Add files via upload

---
+++ MasterPages/MPublicFace.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CA4_10385012
+{
+    public partial class MPublicFace : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
